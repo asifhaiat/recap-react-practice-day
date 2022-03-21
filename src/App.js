77a283f467699f5d9b02e699b98f8d235f470dbd
asifhaiat <asifhaiat67465@gmail.com>
2022-03-21 12:11:42 +0600

    similar in loop different in data

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name="Tangail" speciality="Chomchom"></District>
+      <District name="Bogura" speciality="Doi"></District>
+      <District name="Natore" speciality="Kacha Golla"></District>
+      <District name="Dhaka" speciality="Mosque"></District>
     </div>
   );
+}
+const districtInformation = {
+  backgroundColor: 'lightGreen',
+  margin: '20px',
+  padding: '20px',
+  borderRadius: '20px',
+  border: '2px solid pink'
+}
+function District(props){
+  return(
+    <div style={districtInformation}>
+      <h3>Name: {props.name}</h3>
+      <h4>Speciality: {props.speciality}</h4>
+    </div>
+  )
 }
 
 export default App;
