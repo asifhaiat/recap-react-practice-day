6ce1736e76447c2859b5a9c408145c104899b343
asifhaiat <asifhaiat67465@gmail.com>
2022-03-21 13:31:29 +0600

    Added event listner, state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
   return (
     <div className="App">
-      <District name="Tangail" speciality="Chomchom"></District>
-      <District name="Bogura" speciality="Doi"></District>
-      <District name="Natore" speciality="Kacha Golla"></District>
-      <District name="Dhaka" speciality="Mosque"></District>
+      <Fruit name="Orange" calorie="1.54" country="Brazil"></Fruit>
+      <Fruit name="Banana" calorie="2.84" country="India"></Fruit>
+      <Fruit name="Coconut" calorie="3.94" country="Indonesia"></Fruit>
+      <Fruit name="Date" calorie="4.74" country="Egypt"></Fruit>
     </div>
   );
 }
-const districtInformation = {
-  backgroundColor: 'lightGreen',
+
+const fruitsStyle = {
+  backgroundColor: 'coral',
   margin: '20px',
   padding: '20px',
-  borderRadius: '20px',
-  border: '2px solid pink'
+  border: '2px solid MistyRose',
+  borderRadius: '20px'
 }
-function District(props){
+function Fruit(props){
+  const [fruit, setFruit] = useState(1)
+  const increaseFruits = () => {
+    const newIncreaseFruits = fruit * 2;
+    setFruit(newIncreaseFruits);
+  }
+
   return(
-    <div style={districtInformation}>
-      <h3>Name: {props.name}</h3>
-      <h4>Speciality: {props.speciality}</h4>
+    <div style={fruitsStyle}>
+      <h3>Name: {props.name}  </h3>
+      <h3>Calorie: {props.calorie}</h3>
+      <h4>Produce Country Name: {props.country}</h4>
+      <h5>Calorie in Power: {fruit}</h5>
+      <button onClick={increaseFruits}>Boost the power in Fruits</button>
     </div>
   )
 }
